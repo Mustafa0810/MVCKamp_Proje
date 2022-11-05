22d9ef14f3136477090a2b7ea703b1c209ecb153
Mustafa0810 <113117330+Mustafa0810@users.noreply.github.com>
2022-11-05 19:08:49 +0300

    Görev2
    40.Ders Sonu

---
+++ MvcKampProje/Controllers/StatisticsController.cs
@@ -0,0 +1,23 @@
+﻿using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcKampProje.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            ViewBag.SumCategori = c.Categories.Count();
+            ViewBag.HeadYazilimSum = c.Headings.Where(x => x.CategoryID == 15).Count();
+            ViewBag.WriterCount = c.Writers.Where(c => c.UserName.Contains("a")).Count();
+            ViewBag.MaxCategoryName = c.Headings.Max(y => y.Category.CategoryName);
+            ViewBag.CategoryMaxMinDifference = c.Categories.Where(x => x.CategoryStatus).Count() - c.Categories.Where(x => !x.CategoryStatus).Count();
+            return View();
+        }
+    }
+}
